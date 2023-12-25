e6a6c0f6c2eeec4109c4f54d5c98528c889cd46b
Giang16 <bao16011304@gmail.com>
2023-12-26 03:40:34 +0700

    start

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,35 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Giang',
+        address: 'Thanh Hoa'
+    }
+
+    handleClick = (event) => {
+        console.log("Click me my botton")
+        console.log("My name is", this.state.name)
+
+        this.setState({
+            name: "Eric"
+        })
+
+        this.setState({
+            address: 'HN'
+        })
+    }
+    handleOnMoverOver(event) {
+        console.log(event.pageX)
+    }
+
     //JSX
     render() {
         return (
-            <div> my first component
-                {Math.random()}
+            <div>
+                I'm {this.state.name} and live in {this.state.address}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMoverOver}>Click me</button>
             </div>
         )
     }
